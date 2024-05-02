b9d25c638d1f533fd502e2eab0332f5ac0b74e6f
SWeRVenet <168746969+SWeRVenet@users.noreply.github.com>
2024-05-02 18:42:38 +0500

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,84 @@
+/*$(".menu-element__link").click(function(){
+    const thisElement = $(this)
+
+    if (thisElement.hasClass('active')){
+        return;
+    }
+
+    const linkHref = thisElement.attr("href");
+    const hrefElement = $(linkHref);
+
+    const paddingTop = parseInt(hrefElement.css("padding-top"));
+    const menuHeight = $(".header-top").height();
+
+    $([document.documentElement, document.body]).animate({
+        scrollTop: hrefElement.offset().top - paddingTop - menuHeight
+    }, 1500);
+
+    $(".menu-element__link.active").removeClass('active');
+    thisElement.addClass('active');
+
+});*/
+
+const menuHeight = document.querySelector(".header-top").clientHeight;
+
+/* Active link after click*/
+document.querySelectorAll(".menu-element__link").forEach(element => {
+    element.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        if (hasClass(this, 'active')) {
+            return;
+        }
+
+        const linkHref = this.getAttribute("href");
+        const hrefElement = document.getElementById(linkHref.replace("#", ""));
+
+        const paddingTop = parseInt(getComputedStyle(document.querySelector('#work')).paddingTop);
+
+        window.scroll({
+            behavior: 'smooth',
+            left: 0,
+            top: hrefElement.offsetTop - paddingTop - menuHeight
+        });
+
+        document.querySelector(".menu-element__link.active").classList.remove('active');
+        this.classList.add('active');
+    });
+});
+
+/* Active link on scroll to section */
+const options = {
+    threshold: 0.3
+}
+const callback = function(entries) {
+    entries.forEach(entry => {
+        const sectionMenuLink = document.querySelector(`.menu-element__link[href='#${entry.target.id}']`);
+        if (entry.isIntersecting && !hasClass(sectionMenuLink, 'active')) {
+            changeActiveMenuElement(sectionMenuLink);
+        }
+    });
+};
+const observer = new IntersectionObserver(callback, options);
+document.querySelectorAll(".observer-section").forEach(element => {
+    observer.observe(element);
+});
+
+// Общие функции
+/**
+ * Меняет активный элемент в пунктах меню
+ * @param element Ссылка в меню
+ */
+function changeActiveMenuElement(element) {
+    document.querySelector(".menu-element__link.active").classList.remove('active');
+    element.classList.add('active');
+}
+
+/**
+ * Проверяет наличие класса у элемента
+ * @param element
+ * @param className
+ */
+function hasClass(element, className) {
+    return element.classList.contains(className);
+}
